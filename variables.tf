cb98f345e8c6cf61f906fe6acfb171623e57e4d3
dhivagar14 <133874270+dhivagar14@users.noreply.github.com>
2023-05-17 17:28:43 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "gcp_project_id" {
+  type = string
+  description = "The id of th eproject in which the resource belongs"
+  default = "<your project id>"
+}
+
+variable "gcp_region" {
+  type = string 
+  default = "us-central"
+}
+
+variable "bucket_name" {
+  type = string
+  default = "dhiva_bucket"
+}
+
+variable "vpc_name" {
+  type = string
+  default = "dhiva_vpc"
+}
+
+variable "gateway_name" {
+  type = string
+  default = "dhiva_gateway"
+}
+variable "gateway_purpose" {
+  type = string 
+  default = "vpc_peering"
+}
+variable "gateway_length" {
+  type = number
+  default = 24
+}
+variable "gcp_router" {
+  type = string
+  default = "my-router"
+}
